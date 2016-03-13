e33991d8942fef1a6625e6ea2d728cc231f5bce6
oroce <robert@oroszi.net>
2016-03-13 15:55:22 +0100

    initial commit

---
+++ bin.js
@@ -0,0 +1,23 @@
+var argv = require('minimist')(process.argv.slice(2), {
+  string: ['event']
+});
+var listener = require('supervisord-eventlistener');
+var execSync = require('child_process').execSync;
+var events = Array.isArray(argv.event) ? argv.event : [argv.event];
+
+
+events.forEach(listen);
+
+function listen(evt) {
+  listener.on(evt, executeCmds);
+}
+
+function executeCmds() {
+  argv._.forEach(execute);
+}
+
+function execute(cmd) {
+  execSync(cmd);
+}
+
+supervisor.listen(process.stdin, process.stdout);
